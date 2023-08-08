8e4f0de04c951dfea3f87ce0814d061626b9d8af
saisrutimaharana <maharanasaisruti@gmail.com>
2023-08-08 20:33:04 +0530

    Sai

--- db/migrate/20230729170434_create_todoings.rb
+++ db/migrate/20230729170434_create_todoings.rb
@@ -1,7 +1,7 @@
 class CreateTodoings < ActiveRecord::Migration[7.0]
   def change
     create_table :todoings do |t|
-      t.text :task
+      
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_06_133837) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_08_145615) do
   create_table "todoings", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.string "task"
   end
 
   create_table "users", force: :cascade do |t|
